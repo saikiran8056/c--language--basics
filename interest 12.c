d851a2102a441e84887c0b1c416dab45472612b1
saikiran8056 <92150409+saikiran8056@users.noreply.github.com>
2021-10-09 14:30:00 +0530

    Add files via upload

---
+++ interest 12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int  height,base,area;
+	printf(\"nenter the height");
+	scanf("%d",&height);
+	printf(\"nenter the base");
+	scanf("%d",&base);
+	area= base*height/2;
+	printf("%d",area);
+}
