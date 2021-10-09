9a73d2af0a6cd39200f8f073f1acd7950526892c
saikiran8056 <92150409+saikiran8056@users.noreply.github.com>
2021-10-09 14:40:44 +0530

    Add files via upload

---
+++ triangle 12.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int area,base,height;
+	printf("\nenter the base");
+	scanf("%d",&base);
+	printf("\nenter the height");
+	scanf("%d",&height);
+	area=base*height/2;
+	printf("\narea of triangle:%d",area);
+	}
