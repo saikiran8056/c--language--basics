19b53c749d5f54b4bcbc26b7078b48a7488e1323
saikiran8056 <92150409+saikiran8056@users.noreply.github.com>
2021-10-09 14:40:02 +0530

    Add files via upload

---
+++ rectangle128.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+	int area,length,breadth;
+	printf("\nenter the length");
+	scanf("%d",&length);
+	printf("\nenter the breadth");
+	scanf("%d",&breadth);
+	area= length*breadth;
+	printf("%d",area);
+}
