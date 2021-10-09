b6f95378b83731c4a485e43cfcf0da2e1410ce92
saikiran8056 <92150409+saikiran8056@users.noreply.github.com>
2021-10-09 14:32:28 +0530

    Add files via upload

---
+++ 1257 realational.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=50,b=100;
+	printf("%d\n",a<b);
+	printf("%d\n",a<=b);
+	printf("%d\n",a>b);
+	printf("%d\n",a>=b);
+	printf("%d\n",a==b);
+	printf("%d\n",a!=b);
+}
